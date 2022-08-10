46239d2c1747b050d112facbc0e54cef35029c82
cha101 <megarecontracool@gmail.com>
2022-08-09 21:20:57 -0400

    Clase 2: Obstaculos_small_cactus

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,12 +1,13 @@
 import pygame
 
-from dinno_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 from pygame.sprite import Sprite
 
 class Dinosaur(Sprite):
     X_POS = 80
     Y_POS = 310
     JUMP_VEL = 8.5 
+    Y_POS_DUCK = 340
 
     def __init__(self):
         self.image = RUNNING[0]
@@ -16,21 +17,29 @@ class Dinosaur(Sprite):
         self.step_index = 0
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
         self.jump_vel = self.JUMP_VEL
 
     def update(self, user_input):
         if self.dino_run:
             self.run()
-        if self.dino_jump:
+        elif self.dino_jump:
             self.jump()
+        elif self.dino_duck:
+            self.duck()
 
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
-        elif not self.dino_run:
+            self.dino_duck = False
+        elif user_input[pygame.K_DOWN] and not self.dino_duck:
+            self.dino_jump = False
+            self.dino_run = False
+            self.dino_duck = True 
+        elif not self.dino_jump and not self.dino_duck:
             self.dino_jump = False 
             self.dino_run = True
-
+            self.dino_duck = False 
 
         if self.step_index >= 10:
             self.step_index = 0
@@ -52,6 +61,14 @@ class Dinosaur(Sprite):
             self.dino_rect.y = self.Y_POS
             self.dino_jump = False
             self.jump_vel = self.JUMP_VEL
+
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS_DUCK
+        self.step_index += 1
+        
 
     def draw(self, screen: pygame.Surface):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
